5ac8f8303cdd5e91bb1431b09292cdfcf2d7b8a2
Patrick Weber <patrickrweber@hotmail.com>
2024-01-23 19:03:33 -0300

    Feat - Structure of the solution

---
+++ AffiliateProducts.Model/DTO/Product/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffiliateProducts.Domain.DTO.Product
+{
+    public class CategoryDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<ProductDTO>? Products { get; }
+    }
+}
